14bbc9eabbba42de273cf6e1c16659644207a161
kovaaalex <kovalenko.alex04@gmail.com>
2024-10-06 23:27:09 +0300

    feat: add worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,4 +1,11 @@
+import path from 'path'
+import access from 'fs/promises'
+import { fileURLToPath } from 'url'
+import { availableParallelism  } from 'os'
+import { Worker } from 'worker_threads'
 const performCalculations = async () => {
+    const filePath = './worker.js'
+    try
     // Write your code here
 };
 
