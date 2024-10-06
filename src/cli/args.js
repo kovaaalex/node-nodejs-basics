9f8cf5130a1a071421a75bf890c61e527d2037fb
kovaaalex <kovalenko.alex04@gmail.com>
2024-10-06 20:06:54 +0300

    feat: add env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,10 @@
 const parseArgs = () => {
-    const cd = process.argv.slice(2)
+    const args = process.argv.slice(2)
+    const modidiedArguments = args.map(arg => arg.replace("--", ""))
+    console.log(modidiedArguments)
+    for(let i = 0; i < modidiedArguments.length; i += 2) {
+        console.log(modidiedArguments[i], 'is', modidiedArguments[i + 1])
+    }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const str = process.env
+    for(let substr in str) {
+        if(substr.startsWith('RSS_')) console.log(`${substr}=${str[substr]}`)
+    }
 };
 
 parseEnv();
