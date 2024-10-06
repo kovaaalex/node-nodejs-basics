5aa128bcc306836fc5fd85f1fff0cd916236d4bd
kovaaalex <kovalenko.alex04@gmail.com>
2024-10-06 23:30:40 +0300

    feat: add main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,12 +1,26 @@
-import path from 'path'
 import access from 'fs/promises'
-import { fileURLToPath } from 'url'
 import { availableParallelism  } from 'os'
 import { Worker } from 'worker_threads'
 const performCalculations = async () => {
-    const filePath = './worker.js'
-    try
-    // Write your code here
+    const filePath = 'src/wt/worker.js'
+    try {
+        await access(filePath)
+        const numberCores = availableParallelism()
+        const workers = []
+        const createWorker = (data) => {
+            return new Promise((resolve) => {
+                const worker = new Worker(workerFile, {workerData: data})
+                worker.on('message', (data) => resolve({status: 'resolved', data}))
+                worker.on('error', () => resolve({status: 'error', data: null}))
+            })
+        }
+        for (let i = 0; i < numberCores; i++)
+            workers.push(createWorker(i + 10))
+        const result = await Promise.all(workers)
+        console.log(result)
+    } catch(error) {
+        console.error(error.message)
+    }
 };
 
 await performCalculations();
